01862b7a7692b71d462f246ec410386d7c229acb
Mikael <47033918+MikkeSpikke@users.noreply.github.com>
2022-02-19 12:13:32 +0200

    lohis hiitee

---
+++ NPCS/Dragon.java
@@ -0,0 +1,9 @@
+public class Dragon extends NPC {
+    public Dragon() {
+        id = "Dragon";
+        hp = 100;
+        accuracy = 50;
+        desc = "A big dragon stands here";
+        name = "A Dragon";
+    }
+}
